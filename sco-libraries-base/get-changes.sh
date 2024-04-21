caacb6bac9dd014cc2e55d81f931b00bc2e44ff6
scomeras94 <scomeras@gmail.com>
2024-04-21 02:19:24 +0200

    UPD - Libraries base scripts

--- sco-libraries-base/get-changes.sh
+++ sco-libraries-base/get-changes.sh
@@ -17,34 +17,13 @@ else
 fi
 
 if [[ $1 == $prod ]] ; then
-    cd sco-angular-components-base
-    pwd
     git pull
     git fetch
     git switch main
-
-    cd ..
-
-    cd sco-nestjs-utilities-base
-    pwd
-    git pull
-    git fetch
-    git switch main
-    cd ..
 else
-    cd sco-angular-components-base
-    pwd
     git pull
     git fetch
     git switch develop
-    cd ..
-
-    cd sco-nestjs-utilities-base
-    pwd
-    git pull
-    git fetch
-    git switch develop
-    cd ..
 fi
 
 if [[ $1 == $prod ]] ; then
--- sco-libraries-base/get-repositories.sh
+++ sco-libraries-base/get-repositories.sh
@@ -2,15 +2,6 @@
 #Para lanzar el script, lanzamos en una terminal posicionada en el fichero el siguiente comando: `./get-repositories.sh`
 echo "Empezando descarga de repositorios..."
 
-#git clone https://github.com/SCO-SantiComeras/sco-libraries-base.git \
-#&& git clone https://github.com/SCO-SantiComeras/sco-libraries-base.git
-
-git clone https://github.com/SCO-SantiComeras/sco-libraries-base.git \
-&& cp -r -a ./sco-libraries-base/* ./ \
-&& cp -r ./sco-libraries-base/.gitignore ./.gitignore \
-&& cp -r ./sco-libraries-base/.git ./.git \
-&& chmod -R 777 ./sco-libraries-base \
-&& sleep 3 \
-&& rm -f -r ./sco-libraries-base
+git clone https://github.com/SCO-SantiComeras/sco-libraries-base.git
 
 echo "Repositorios descargados correctamente"
